16244639f288599b107242cfe555e9c92cf265b2
Sam Wells <samwells95@gmail.com>
2021-04-22 13:40:10 +0100

    Add lifecycle policy for ECR

--- infrastructure/ecr-repository.tf
+++ infrastructure/ecr-repository.tf
@@ -2,3 +2,26 @@ resource "aws_ecr_repository" "create-repository" {
   name                 = "user-api"
   image_tag_mutability = "MUTABLE"
 }
+
+resource "aws_ecr_lifecycle_policy" "create-repository-policy" {
+  repository = aws_ecr_repository.create-repository.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire images older than 14 days",
+            "selection": {
+                "tagStatus": "any",
+                "countType": "imageCountMoreThan",
+                "countNumber": 14
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
